122139365edc50ffad5f77494b595a13ca40d7d0
sahala-123 <sahalapr@gmail.com>
2024-02-01 13:51:34 +0530

    assignment1

---
+++ calc.py
@@ -0,0 +1,16 @@
+a=int(input("enter the no"))
+b=int(input("enter the no"))
+y=input("enter the operator")
+if(y=="+"):
+    c=a+b
+    print(c)
+if(y=="-"):
+    c=a-b
+    print(c)
+if(y=="*"):
+    c=a*b
+    print(c)
+if(y=="/"):
+    c=a/b
+    print(c)
+
---
+++ username.py
@@ -0,0 +1,2 @@
+a=input("enter name")
+print("Hello",a)
